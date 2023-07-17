317605005b8f1369ee5e4410b1d88b8939c44a25
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-07-18 00:39:54 +0600

    footer

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,11 +1,13 @@
 import {Outlet} from 'react-router-dom';
 import Navbar from "../components/Home/Navbar/Navbar";
+import Footer from '../components/Home/Footer/Footer';
 
 const Main = () => {
     return (
         <>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </>
     );
 };
